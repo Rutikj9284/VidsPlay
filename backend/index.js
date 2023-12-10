b7bd78dd47c8e47af0baacafd17067efc91e4a5a
Rutikj9284 <137148058+Rutikj9284@users.noreply.github.com>
2023-12-10 12:17:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,20 @@ app.use(express.json());
 app.use(cookieParser());
 dotenv.config();
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  const allowedOrigins = ['https://65755e2b91acff70e8ca0541--symphonious-sfogliatella-c085ee.netlify.app/'];
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   next();
 });
+
 app.use(cors({ origin: '*' }));
 
 const jwtSecret = process.env.JWT_SECRET;
